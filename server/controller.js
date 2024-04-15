d11f8b739237b6cf92bd429c9747fb3cf21e2c2b
Beau Pomeroy <beaupomeroy@gmail.com>
2024-04-14 23:12:26 -0700

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,37 @@
+const results = [];
+
+module.exports = {
+
+    getRetirementQuote: (req, res) => {
+        const retirementQuote = ["Retirement is more fun than work, and you never have to make excuses. -Unknown",
+                                 "Retirement: That\’s when you return from work one day and say, \"Hi, Honey, I\'m home\" — forever. – Gene Perret", 
+                                 "Don\’t simply retire from something; have something to retire to. – Harry Emerson Fosdick"];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * retirementQuote.length);
+        let randomRetirementQuote = retirementQuote[randomIndex];
+      
+        res.status(200).send(randomRetirementQuote);
+    },
+    createRetirement: (req, res) => {
+        let retirement = req.body
+        console.log(req.body)
+        if (!retirement || typeof retirement !== 'object'){
+            return res.status(400).json({ success: false, message: 'Error' });
+        }
+        results.push(retirement)
+        res.status(200).json({ success: true, message: 'success',results:results });
+        },
+
+    deleteRetirementData: (req, res) => {
+            const { index } = req.params
+            // let index = results(elem => elem === req.params.index)
+            if (index !== -1) {
+                results.splice(index, 1);
+                res.status(200).json(results);
+            } else {
+                res.status(400).json({ error: 'Index not found' });
+        }  
+    } 
+}
+
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const {} = require('./controller.js')
+
+app.use(cors());
+app.use(express.json());
+
+const { createRetirement, getRetirementQuote, deleteRetirementData } = require('./controller')
+
+app.get("/api/quote", getRetirementQuote);
+app.post("/api/createRetirement", createRetirement);
+app.delete("/api/delete/:index", deleteRetirementData);
+app.listen(4000, () => console.log("Server running on 4000"));
